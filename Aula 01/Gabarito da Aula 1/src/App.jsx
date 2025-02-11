5407145264dea278253dedaaca4b18329cdaa32b
Fabiano Hermann <fabiano29hermanngba@gmail.com>
2025-02-11 09:45:45 -0300

    up

---
+++ Aula 01/Gabarito da Aula 1/src/App.jsx
@@ -0,0 +1,9 @@
+
+function App() {
+  console.log("Olá, Mundo!");
+
+  return <h1>Olá, Mundo!</h1>;
+  
+}
+
+export default App
---
+++ Aula 01/Gabarito da Aula 1/src/App2.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App2 = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App2
+ //rafce
+ // Arrow Function
+//const App2 = () => <h1>Olá, Mundo!</h1>;
+
